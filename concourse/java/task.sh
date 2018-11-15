8f5e1de5c1b692274c41a620a702d40310b8bd43
kleppa <Jarandwaagekleppa@gmail.com>
2018-11-15 19:59:52 +0100

    x

--- concourse/java/task.sh
+++ concourse/java/task.sh
@@ -28,5 +28,7 @@ cat > ${M2_HOME}/settings.xml <<EOF
       <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
 </settings>
 EOF
-mvn -f source/pom.xml install 
+mvn -f source/pom.xml install
+mv ./source/target/herokupipe-example-0.0.1-SNAPSHOT.jar ./jar-file/app.jar
+mv ./source/Dockerfile ./jar-file/Dockerfile
 echo -e "${GREEN}${CHECK} Maven install${NC}"
